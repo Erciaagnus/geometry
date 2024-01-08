40a2319fb6e4a39bdb1d9ba578f5f2233ab88c22
Erciaagnus <henricus0973@gmail.com>
2024-01-08 17:16:36 +0900

    Applied skeleton code for point2d

--- include/geometry/point2d.hpp
+++ include/geometry/point2d.hpp
@@ -12,8 +12,117 @@
 //@copyright Copyright (c) 2024 Programmers All Rights Reserved.
 // Authors: JeongHyeok Lim
 
+// header Guard
 #ifndef Programmers_GEOMETRY_POINT_2_HPP_
 #define Programmers_GEOMETRY_POINT_2_HPP_
 
-#endif // Programmers_GEOMETRY_POINT_2_HPP_
+namespace programmers::geometry {
 
+/**
+ * @brief Point class with 2-dimention
+ */
+class Point2D {
+public:
+  /**
+   * @brief Construct a new Point 2 D object
+   * @param x Double type x coordinate value
+   * @param y Double type y coordinate value
+   */
+  Point2D() = default;
+  /**
+   * @brief Construct a new Point 2 D object
+   * @param x Double type x coordinate value
+   * @param y Double type y coordinate value
+   */
+  Point2D(double x, double y);
+  /**
+   * @brief Copy construct a new Point 2 D object with other Point2D object
+   * @param other Point2D object
+   */
+  Point2D(const Point2D &other) = default;
+  /**
+   * @brief Move construct a new Point 2 D object with other Point2D object
+   * @param other Point2D object
+   */
+  Point2D(Point2D &&other) noexcept = default;
+
+  /**
+   * @brief Destroy the Point 2 D object
+   *
+   */
+  virtual ~Point2D() = default;
+  /**
+   * @brief Copy assignment operator
+   * @param other Point2D object
+   * @return Point2D& Reference of Point2D object
+   */
+  // Point2D new_one = old_one;
+  // new_one.GetX();
+  // old_one.GetX();
+
+  auto operator=(const Point2D &other) -> Point2D & = default;
+  /**
+   * @brief Move assignment operator
+   * @param other other Point2D object
+   * @return Point2D& Reference of Point2D object
+   */
+  // new_one.GetX();
+  // old_one.GetX(); impossible xxxxxxxxxx
+  auto operator=(Point2D &&other) -> Point2D & = default;
+  /**
+   * @brief Calculate distance between this point and target poin
+   * @param target Other Point2D object to calculate distance
+   * @return double Euclidean distance between this point and target point
+   */
+
+  auto CalculateDistance(const Point2D &target) const -> double;
+  /**
+   * @brief Calculate distance between lhs point and rhs point
+   * @param lhs Left hand side Point2D object
+   * @param rhs Right hand side Point2D object
+   * @return double Euclidean distance between lhs point and rhs point
+   */
+  [[nodiscard]] static auto CalculateDistance(const Point2D &lhs,
+                                              const Point2D &rhs) -> double;
+  /**
+   * @brief Set x coordinate value
+   * @param x Double type input x coordinate value
+   */
+  void SetX(double x);
+  /**
+   * @brief Set y coordinate value
+   * @param y Double type input y coordinate value
+   */
+  void SetY(double y);
+
+  /**
+   * @brief Get x coordinate value of this point
+   * @return double x coordinate value of this point
+   */
+
+  [[nodiscard]] auto GetX() const -> double;
+  /**
+   * @brief Get y coordinate value of this point
+   * @return double y coordinate value of this point
+   */
+
+  [[nodiscard]] auto GetY() const -> double;
+
+  auto operator+(const Point2D &other) const -> Point2D;
+  auto operator-(const Point2D &other) const -> Point2D;
+
+  auto operator+=(const Point2D &other) const -> Point2D;
+  auto operator-=(const Point2D &other) const -> Point2D;
+
+  auto operator==(const Point2D &other) const -> Point2D;
+  auto operator!=(const Point2D &other) const -> Point2D;
+
+protected:
+private:
+  double x_{0.0}; ///< x coordinate
+  double y_{0.0}; ///< y coordinate
+};
+
+} // namespace programmers::geometry
+
+#endif // Programmers_GEOMETRY_POINT_2_HPP_
