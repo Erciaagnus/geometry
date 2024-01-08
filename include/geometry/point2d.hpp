2867d8fc441e2a07e5ecf2ad7f3adfd58148d0a1
Erciaagnus <henricus0973@gmail.com>
2024-01-08 18:06:48 +0900

    Added explain doxygen form for operators

--- include/geometry/point2d.hpp
+++ include/geometry/point2d.hpp
@@ -1,11 +1,11 @@
 /**
- * @file point2d.hpp
- * @author JeongHyeok Lim (henricus0973@korea.ac.kr)
- * @brief Point class declaration with 2-dimention
- * @version 0.1
- * @date 2024-01-07
+ *@file point2d.hpp
+ *@author JeongHyeok Lim (henricus0973@korea.ac.kr)
+ *@brief Point class declaration with 2-dimention
+ *@version 0.1
+ *@date 2024-01-07
  *
- * @copyright Copyright (c) 2024 Programmers All Rights Reserved.
+ *@copyright Copyright (c) 2024 Programmers All Rights Reserved.
  *
  */
 
@@ -70,7 +70,7 @@ public:
   // old_one.GetX(); impossible xxxxxxxxxx
   auto operator=(Point2D &&other) -> Point2D & = default;
   /**
-   * @brief Calculate distance between this point and target poin
+   * @brief Calculate distance between this point and target point
    * @param target Other Point2D object to calculate distance
    * @return double Euclidean distance between this point and target point
    */
@@ -108,13 +108,64 @@ public:
 
   [[nodiscard]] auto GetY() const -> double;
 
+  /**
+   * @brief Over loaded addition operator for Point2D
+   * @param other The point2D object to be added
+   * @return The result of the addition as a new Point2D object
+   */
   auto operator+(const Point2D &other) const -> Point2D;
+
+  /**
+   * @brief Overloaded subtraction operator for Point2D.
+   *
+   * This operator performs element-wise subtraction of two Point2D objects.
+   *
+   * @param other The Point2D object to be subtracted.
+   * @return The result of the subtraction as a new Point2D object.
+   */
+
   auto operator-(const Point2D &other) const -> Point2D;
 
-  auto operator+=(const Point2D &other) const -> Point2D;
-  auto operator-=(const Point2D &other) const -> Point2D;
+  /**
+   * @brief Overloaded addition assignment operator for Point2D.
+   *
+   * This operator performs element-wise addition and assigns the result to the
+   * current Point2D object.
+   *
+   * @param other The Point2D object to be added.
+   * @return A reference to the modified Point2D object.
+   */
 
+  auto operator+=(const Point2D &other) const -> Point2D;
+
+  /**
+   * @brief Overloaded subtraction assignment operator for Point2D.
+   *
+   * This operator performs element-wise subtraction and assigns the result to
+   * the current Point2D object.
+   *
+   * @param other The Point2D object to be subtracted.
+   * @return A reference to the modified Point2D object.
+   */
+
+  auto operator-=(const Point2D &other) const -> Point2D;
+  /**
+   * @brief Overloaded equality operator for Point2D.
+   *
+   * This operator checks if two Point2D objects are equal element-wise.
+   *
+   * @param other The Point2D object to be compared for equality.
+   * @return True if the objects are equal, false otherwise.
+   */
   auto operator==(const Point2D &other) const -> Point2D;
+  /**
+   * @brief Overloaded inequality operator for Point2D.
+   *
+   * This operator checks if two Point2D objects are not equal element-wise.
+   *
+   * @param other The Point2D object to be compared for inequality.
+   * @return True if the objects are not equal, false otherwise.
+   */
   auto operator!=(const Point2D &other) const -> Point2D;
 
 protected:
