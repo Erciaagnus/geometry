d4be32703b313bc9737217c52acd0246ee3714a2
Erciaagnus <henricus0973@gmail.com>
2024-01-09 17:08:00 +0900

    Added test function for repo

---
+++ test/unit/point2d.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2024 Programmers, All Rights Reserved
+// Authors: Lim Jeong Hyeok
+
+
+#include <limits>
+
+#include <stdexcept>
+#include "gtest/gtest.h"
+
+namespace programmers::geometry {
+}
