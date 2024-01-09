a68ec659273a85b040691febf1b2cf07e9cfd2b3
Erciaagnus <henricus0973@gmail.com>
2024-01-09 17:11:14 +0900

    Added point2d test skeleton

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -1,11 +1,15 @@
 // Copyright (C) 2024 Programmers, All Rights Reserved
 // Authors: Lim Jeong Hyeok
 
-
+#include "geometry/point2d.hpp"
 #include <limits>
 
 #include <stdexcept>
 #include "gtest/gtest.h"
 
 namespace programmers::geometry {
+  TEST(GeometryPoint2D, Constructors)
+  {
+
+  }
 }
